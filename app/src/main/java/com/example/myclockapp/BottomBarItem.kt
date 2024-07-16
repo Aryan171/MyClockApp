76d452257116d025c5d45bb11cada4ef8c074d80
Aryan171 <62366692+Aryan171@users.noreply.github.com>
2024-07-16 11:32:56 +0530

    Added a bottom bar to the app along with some place holder icons

---
+++ app/src/main/java/com/example/myclockapp/BottomBarItem.kt
@@ -0,0 +1,8 @@
+package com.example.myclockapp
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem(
+    val title: String,
+    val icon: ImageVector
+)
--- app/src/main/java/com/example/myclockapp/MainActivity.kt
+++ app/src/main/java/com/example/myclockapp/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.myclockapp.ui.theme.MyClockAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +32,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyClockAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val bottomBarItems = listOf(
+                    BottomBarItem(
+                        title = "Timer",
+                        icon = Icons.Default.KeyboardArrowDown
+                    ),
+                    BottomBarItem(
+                        title = "Clock",
+                        icon = Icons.Default.CheckCircle
+                    ),
+                    BottomBarItem(
+                        title = "Stop Watch",
+                        icon = Icons.Default.Home
                     )
+                )
+
+                val navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+                        BottomBar(
+                            navController,
+                            bottomBarItems
+                        )
+                    }
+                ) {
+                    Text("Screen Goes Here", modifier = Modifier.padding(it))
                 }
             }
         }
@@ -31,17 +65,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun BottomBar(
+    navController: NavHostController,
+    bottomBarItems: List<BottomBarItem>
+) {
+    var selectedIndex by remember {
+        mutableIntStateOf(1)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyClockAppTheme {
-        Greeting("Android")
+    NavigationBar(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        for(i in bottomBarItems.indices) {
+            NavigationBarItem(
+                selected = i == selectedIndex,
+                onClick = {
+                    selectedIndex = i
+                },
+                icon = {
+                    Icon(
+                        imageVector = bottomBarItems[i].icon,
+                        contentDescription =  bottomBarItems[i].title,
+                        tint = if(selectedIndex == i) Color.White else Color.Gray
+                    )
+                },
+                label = {
+                    Text(bottomBarItems[i].title)
+                }
+            )
+        }
     }
 }
